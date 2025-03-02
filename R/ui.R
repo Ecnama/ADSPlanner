1369443ac9b0b7aa997cd360d406f6ad392b1191
pixup1 <pixup1@yahoo.com>
2025-03-02 23:11:46 +0100

    Improve selection

--- R/ui.R
+++ R/ui.R
@@ -25,11 +25,11 @@ ui <- page_sidebar(
         nav_panel(
             "Affectations d\u00E9partements",
             div(
-                actionButton("assign_depart_hard_1", "Affectation \"dure\" voeu 1", width = 200),
-                actionButton("assign_depart_hard_2", "Affectation \"dure\" voeu 2", width = 200),
-                actionButton("assign_depart_hard_3", "Affectation \"dure\" voeu 3", width = 200),
+                actionButton("assign_depart_erase", "Effacer toutes affectations", width = 180),
+                actionButton("assign_depart_hard_1", "Affectation \"dure\" voeu 1", width = 180),
+                actionButton("assign_depart_hard_2", "Affectation \"dure\" voeu 2", width = 180),
+                actionButton("assign_depart_hard_3", "Affectation \"dure\" voeu 3", width = 180),
                 actionButton("assign_depart_real", "Affectation r\u00E9elle voeux restants", width = 200),
-                actionButton("assign_depart_erase", "Effacer toutes affectations", width = 200),
             ),
             uiOutput("aff_depart"),
         ),
