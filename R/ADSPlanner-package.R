90f096d7a0475aacfdf41757eb26058139746659
pixup1 <pixup1@yahoo.com>
2025-03-03 13:03:14 +0100

    Fix stupid warning

--- R/ADSPlanner-package.R
+++ R/ADSPlanner-package.R
@@ -1,6 +1,7 @@
 # This file is used to define the imports that will be updated by roxygen2 when running `devtools::document()`.
 
-#' @import shiny
+# Workaround to avoid a weird warning
+#' @rawNamespace import(shiny, except=c(dataTableOutput, renderDataTable))
 #' @import DT
 #' @import bslib
 #' @import openxlsx
