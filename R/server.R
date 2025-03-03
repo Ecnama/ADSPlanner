e31e6a9ef2c2ec015dbf926e0a5ce405655cb304
pixup1 <pixup1@yahoo.com>
2025-03-03 11:16:47 +0100

    Clean up

--- R/server.R
+++ R/server.R
@@ -1,9 +1,8 @@
 library(shiny)
-library(DT)
 
 source("R/input.R")
 source("R/affectations.R")
-source("R/DTutilities.R")
+source("R/tables.R")
 
 #' Backend server logic
 #'
@@ -36,119 +35,5 @@ server <- function(input, output) {
         }
     )
 
-    # output$filter_filiere <- renderUI(
-    #     selectInput(
-    #         "select_filiere",
-    #         "Fili\u00E8res :",
-    #         df()$Filiere,
-    #         multiple = TRUE,
-    #     )
-    # )
-
-    filtered_df <- reactive({
-        filtered_df <- df()
-        if (length(input$select_filiere) > 0) {
-            filtered_df <- filtered_df[filtered_df$Filiere %in% input$select_filiere, ]
-        }
-        filtered_df
-    })
-
-    output$vis_table <- DT::renderDataTable(
-        {
-            filtered_df()[, !grepl("^Aff", names(filtered_df()))]
-        },
-        extensions = c("Scroller"),
-        filter = "top",
-        selection = "none",
-        server = FALSE
-    )
-
-    output$vis <- renderUI({
-        if (is.null(input$file)) {
-            HTML('<div style="display: flex; justify-content: center; align-items: center; height: 100vh; font-weight: bold;">Veuillez charger un fichier pour commencer.</div>')
-        } else {
-            DT::dataTableOutput("vis_table")
-        }
-    })
-
-    output$aff_depart_table <- DT::renderDataTable(
-        {
-            filtered_df_depart <- df()
-            filtered_df_depart[["D\u00E9partements affect\u00E9s"]] <- apply(filtered_df_depart[, grepl("^Aff_depart_", names(filtered_df_depart))], 1, function(x) {
-                x <- x[!is.na(x)]
-                if (length(x) == 0) {
-                    return(NA)
-                }
-                paste(x, collapse = ", ")
-            })
-            filtered_df_depart[, !grepl("^Aff", names(filtered_df_depart))]
-        },
-        filter = "top",
-        extensions = c("Select", "Buttons", "Scroller"),
-        options = list(
-            select = list(style = "multi+shift", items = "row"),
-            dom = '<"top"lfB>rt<"bottom"ip><"clear">',
-            buttons = dt_select_deselect_buttons,
-            deferRender = TRUE,
-            scrollY = 320,
-            scroller = TRUE
-        ),
-        selection = "none",
-        server = FALSE
-    )
-
-    output$aff_depart <- renderUI({
-        if (is.null(input$file)) {
-            HTML('<div style="display: flex; justify-content: center; align-items: center; height: 100vh; font-weight: bold;">Veuillez charger un fichier pour commencer.</div>')
-        } else {
-            c(
-                DT::dataTableOutput("aff_depart_table"),
-                "Cliquez sur les lignes pour les s\u00E9lectionner. Les modifications ne s'appliqueront qu'aux lignes s\u00E9lectionn\u00E9es."
-            )
-        }
-    })
-}
-
-#' Function used by server to handle affectations
-#'
-#' @param input Input data from the frontend
-#' @param output Output data the frontend will receive
-#' @param df The reactive data frame of students's wishes and affectations
-handle_affectations <- function(input, output, df) {
-    try_affectation <- function(number) {
-        if (is.null(df())) {
-            showNotification("Aucun fichier charg\u00E9.", type = "warning")
-            return()
-        }
-
-        df(assign_depart_hard(df(), input$aff_depart_table_rows_selected, number))
-
-        showNotification(paste("D\u00E9partements des voeux ", number, " affect\u00E9s."), type = "message")
-    }
-
-    observeEvent(input$assign_depart_hard_1, try_affectation(1))
-
-    observeEvent(input$assign_depart_hard_2, try_affectation(2))
-
-    observeEvent(input$assign_depart_hard_3, try_affectation(3))
-
-    observeEvent(input$assign_depart_real, {
-        if (is.null(df())) {
-            showNotification("Aucun fichier charg\u00E9.", type = "warning")
-            return()
-        }
-
-        showNotification("Not implemented yet.", type = "warning")
-    })
-
-    observeEvent(input$assign_depart_erase, {
-        if (is.null(df())) {
-            showNotification("Aucun fichier charg\u00E9.", type = "warning")
-            return()
-        }
-
-        df(assign_depart_erase(df(), input$aff_depart_table_rows_selected))
-
-        showNotification("Affectations de d\u00E9partements effac\u00E9es.", type = "message")
-    })
+    display_tables(input, output, df)
 }
---
+++ R/tables.R
@@ -0,0 +1,69 @@
+library(DT)
+
+source("R/DTutilities.R")
+
+#' Function used by server to display tables
+#'
+#' @param input Input data from the frontend
+#' @param output Output data the frontend will receive
+#' @param df The reactive data frame of students's wishes and affectations
+display_tables <- function(input, output, df) {
+    # UI definitions
+
+    output$vis <- renderUI({
+        if (is.null(input$file)) {
+            HTML('<div style="display: flex; justify-content: center; align-items: center; height: 100vh; font-weight: bold;">Veuillez charger un fichier pour commencer.</div>')
+        } else {
+            DT::DTOutput("vis_table")
+        }
+    })
+
+    output$aff_depart <- renderUI({
+        if (is.null(input$file)) {
+            HTML('<div style="display: flex; justify-content: center; align-items: center; height: 100vh; font-weight: bold;">Veuillez charger un fichier pour commencer.</div>')
+        } else {
+            c(
+                DT::DTOutput("aff_depart_table"),
+                "Cliquez sur les lignes pour les s\u00E9lectionner. Les modifications ne s'appliqueront qu'aux lignes s\u00E9lectionn\u00E9es."
+            )
+        }
+    })
+
+    # Table definitions
+
+    output$vis_table <- DT::renderDT(
+        {
+            df()[, !grepl("^Aff", names(df()))]
+        },
+        extensions = c("Scroller"),
+        filter = "top",
+        selection = "none",
+        server = FALSE
+    )
+
+    output$aff_depart_table <- DT::renderDT(
+        {
+            df_depart <- df()
+            df_depart[["D\u00E9partements affect\u00E9s"]] <- apply(df_depart[, grepl("^Aff_depart_", names(df_depart))], 1, function(x) {
+                x <- x[!is.na(x)]
+                if (length(x) == 0) {
+                    return(NA)
+                }
+                paste(x, collapse = ", ")
+            })
+            df_depart[, !grepl("^Aff", names(df_depart))]
+        },
+        filter = "top",
+        extensions = c("Select", "Buttons", "Scroller"),
+        options = list(
+            select = list(style = "multi+shift", items = "row"),
+            dom = '<"top"lfB>rt<"bottom"ip><"clear">',
+            buttons = dt_select_deselect_buttons,
+            deferRender = TRUE,
+            scrollY = 320,
+            scroller = TRUE
+        ),
+        selection = "none",
+        server = FALSE
+    )
+}
