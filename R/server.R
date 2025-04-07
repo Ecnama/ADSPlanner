2515b980f1cb8867fd2970545577a44b24170c2e
Mari <marie.revol@insa-rennes.fr>
2025-04-07 21:44:38 +0200

    capacities_counters_table normalement ca compte mais jsp comment tester

--- R/server.R
+++ R/server.R
@@ -61,6 +61,14 @@ server <- function(input, output) {
 
     display_tables(input, output, df)
 
+    capacity_full_shown <- reactiveVal(FALSE)
+
+    output$capacity_full <- renderText({
+        if (capacity_full_shown()) {
+            return("La capacité d'un département est pleine : changez de méthode d'affectation.")
+        }
+    })
+
     output$capacity_full <- renderText({
         if (is.null(remaining_capacities()) || length(remaining_capacities()) == 0) {
             return("")
