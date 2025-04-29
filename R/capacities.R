d3654bb440261ce4cc2438984ba7b1b131801f88
Mari <marie.revol@insa-rennes.fr>
2025-04-29 15:40:32 +0200

    handle_capacities on serveur

--- R/capacities.R
+++ R/capacities.R
@@ -1,11 +1,9 @@
 #' Handle capacities on server
 #'
-#' @param input Input data from the frontend
-#' @param output Output data the frontend will receive
 #' @param df The reactive data frame of students's wishes and affectations
 #' @param capacities The reactive table of each departments capacities
 #' @param remaining_capacities The reactive table of the remaining capacities in each department
-handle_capacities <- function(input, output, df, capacities, remaining_capacities) {
+handle_capacities <- function(df, capacities, remaining_capacities) {
     observe({
         if (!is.null(df())) {
             updated_df <- df()
--- R/server.R
+++ R/server.R
@@ -38,6 +38,8 @@ server <- function(input, output) {
 
     remaining_capacities <- reactiveVal(capacities())
 
+    handle_capacities(df, capacities, remaining_capacities)
+
     output$download_button <- renderUI({
         downloadButton("download", paste("T\u00E9l\u00E9charger ", input$download_name, ".xlsx", sep = ""))
     })
